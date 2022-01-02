0ce4eeb390d597c40dd6dcf082d382c2ee4958b7
tjscks0510 <tjscks0510@gmail.com>
2022-01-03 01:54:26 +0900

    [Seongchan, ticket-#] [Title] Styling for Responsive [Description] added the media query CSS code for Responsive [Relevant] -

---
+++ src/javascripts/main.js
@@ -0,0 +1,7 @@
+// Responsive
+const toggleBtn = document.querySelector(".navbar__toggleBtn");
+const menu = document.querySelector(".navbar__menu");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
